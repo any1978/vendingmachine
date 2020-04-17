46912bd665f124c3aad0b2b5b79d4fe7af947518
katsuhiro.tsukinoki@gmail.com <slowandlow3104>
2020-04-17 12:59:51 +0900

    合計金額追加

--- lib/vending_machine.rb
+++ lib/vending_machine.rb
@@ -1,20 +1,48 @@
 # require 'minitest/autorun'
 
-require "./lib/drink.rb"
+# require "./lib/drink.rb"
+
+class Drink
+  attr_reader :name,:price
+
+  # def initialize(attributes)
+  #   @name = attributes.fetch(:name,'nil')
+  #   @price = attributes.fetch(:price,'nil')
+  # end
+
+  def initialize(name, price)
+    @name = name
+    @price = price
+  end
+
+  def self.cola
+    self.new(:cola,120)
+  end
+
+  # def self.redbull
+  #   self.new(:redbull,200)
+  # end
+
+  # def self.water
+  #   self.new(:water,100)
+  # end
+
+end
+
+
 
 class VendingMachine
   AVAILABLE_MONEY = [10, 50, 100, 500, 1000].freeze
 
-  # これで投入合計額、売上金額をメソッドで呼び出せる。
-  attr_reader :total,:sale_amount
+  # これで投入合計額、売上金額をメソッドで呼び出せる。v1
+  attr_reader :total,:sale_amount, :drink_table
     
     #初期値
     def initialize
       @total = 0
       @sale_amount = 0
-      @drink_table = {}
-      5.times { stock(Drink.cola) }
-      # @stock_coin =    #お釣り計算用　#オブジェクトを5個にする
+      @drink_table = {name: :cola, price:120, stock:5}
+      # v1.drink_table[:stock]で確認できる
     end
   
     #お金を受け取る 繰り返し
@@ -26,7 +54,7 @@ class VendingMachine
       end
     end
 
-  #払い戻し
+    #払い戻し
     def refund     
       if @total == 0
         puts "払い戻すお金はありません"
@@ -35,77 +63,93 @@ class VendingMachine
         @total = 0
       end
     end
-  
 
-    def total
+
+    def money_check
+      @total >= drink_table[:price] 
     end
 
-    def drink_table
-    end
-
-    def sale_amount
-    end
-
-    #在庫管理 stockにコーラ　が
+    # 在庫管理
     def stock
-      
+      @stock = drink_table[:stock]
     end
+    
+   
+    def purchase
+      if money_check
+        # puts "購入できます"
+        @total -= drink_table[:price]
+        @drink_table[:stock] -= 1
+        @sale_amount += drink_table[:price]
+        # puts "残りは、#{@drink_table[:stock]}本です"
+        # @stock -= 1
+      else
+        puts "購入できません"
+      end
+    end
+
+
+    def sales_amount
+    end
+
+
+  end
 
 
     #購入する　  #投入金額は商品金額以上か？ #お釣り&在庫確認！！
-    def purchase
-        # puts "メニューの番号を選択してください"
-        # order = gets.chomp.to_i
-        # selected_menu = menus[order]
-        # puts "選択したメニュー: #{selected_menu.name}"
-        # puts "料金は#{selected_menu.price}円です"
-        # while true　　#purchases.each do |purchase|  #購入作業を繰り返す
-        # info[:price] <= total && info[:stock].size > 0
+#     def purchase
+#         # puts "メニューの番号を選択してください"
+#         # order = gets.chomp.to_i
+#         # selected_menu = menus[order]
+#         # puts "選択したメニュー: #{selected_menu.name}"
+#         # puts "料金は#{selected_menu.price}円です"
+#         # while true　　#purchases.each do |purchase|  #購入作業を繰り返す
+#         # info[:price] <= total && info[:stock].size > 0
 
-        if @total >= @drink_table[:cola][:price][:size]
-          #購入できる
-          @total -= @drink_table[:cola][:price]  #total金額の減少
-          @drink_table[:cola][:stock] -= 1     #在庫の減少
+#         if @total >= @drink_table[:cola][:price][:size]
+#           #購入できる
+#           @total -= @drink_table[:cola][:price]  #total金額の減少
+#           @drink_table[:cola][:stock] -= 1     #在庫の減少
 
-          puts "コーラを購入しました"
-          puts "もう一本購入しますか？"
-          @sales_amount += @drink_table[:cola][:price]
-          #お釣りが必要な場合は減少させる
-          # @stock_coin = 
+#           puts "コーラを購入しました"
+#           puts "もう一本購入しますか？"
+#           @sales_amount += @drink_table[:cola][:price]
+#           #お釣りが必要な場合は減少させる
+#           # @stock_coin = 
 
-          #金額が足りない
-        else 
-          puts "金額が足りませんので商品を購入できません"
-          purchase = false
-          break
-        end
-      end
-    end
+#           #金額が足りない
+#         else 
+#           puts "金額が足りませんので商品を購入できません"
+#           purchase = false
+#           break
+#         end
+#       end
+#     end
     
 
 
-#     #ドリンクラインナップ
-#     drink1 = Drink.new(name: "cola", price: 120)
-#     drink2 = Drink.new(name: "water", price: 100)
-#     menus = [drink1, drink2]
+# #     #ドリンクラインナップ
+# #     drink1 = Drink.new(name: "cola", price: 120)
+# #     drink2 = Drink.new(name: "water", price: 100)
+# #     menus = [drink1, drink2]
 
-#     def info
-#       return "#{self.name} #{self.price}円 "
-#     end
+# #     def info
+# #       return "#{self.name} #{self.price}円 "
+# #     end
   
-#     #メニュー表記
-#     index = 0
-#     menus.each do |menu|
-#       puts "#{index}. #{drink.info}"
-#       index += 1
+# #     #メニュー表記
+# #     index = 0
+# #     menus.each do |menu|
+# #       puts "#{index}. #{drink.info}"
+# #       index += 1
+# #     end
+
+
+
+#     #在庫ストック減少
+#     def decrease_drink_stock()       
+#       @drink_table[:cola][:stock] -= 1
 #     end
-
-
-
-    #在庫ストック減少
-    def decrease_drink_stock()       
-      @drink_table[:cola][:stock] -= 1
-    end
 
 
 
